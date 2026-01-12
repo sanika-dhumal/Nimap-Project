7c4b96cb360e3100f41e3fd6e2d8a40c9d734aac
sanika-dhumal <sanu882002@gmail.com>
2026-01-12 11:04:07 +0530

    project is completed

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,21 +1,21 @@
 const db = require("../config/db");
 
 exports.getAll = (callback) => {
-  db.query("SELECT * FROM Category ORDER BY CategoryId ASC", callback);
+  db.query("select * FROM Category order BY CategoryId ASC", callback);
 };
 
 exports.getById = (id, callback) => {
-  db.query("SELECT * FROM Category WHERE CategoryId=?", [id], callback);
+  db.query("select * from Category where CategoryId=?", [id], callback);
 };
 
 exports.create = (name, callback) => {
-  db.query("INSERT INTO Category(CategoryName) VALUES (?)", [name], callback);
+  db.query("insert into Category(CategoryName) values (?)", [name], callback);
 };
 
 exports.update = (id, name, callback) => {
-  db.query("UPDATE Category SET CategoryName=? WHERE CategoryId=?", [name, id], callback);
+  db.query("update Category SET CategoryName=? where CategoryId=?", [name, id], callback);
 };
 
 exports.remove = (id, callback) => {
-  db.query("DELETE FROM Category WHERE CategoryId=?", [id], callback);
+  db.query("delete FROM Category where CategoryId=?", [id], callback);
 };
--- models/productModel.js
+++ models/productModel.js
@@ -2,30 +2,30 @@ const db = require("../config/db");
 
 exports.getPaginated = (pageSize, offset, callback) => {
   const sql = `
-    SELECT p.ProductId, p.ProductName, c.CategoryId, c.CategoryName
-    FROM Product p
-    JOIN Category c ON p.CategoryId=c.CategoryId
-    ORDER BY p.ProductId ASC
-    LIMIT ? OFFSET ?`;
+    select p.ProductId, p.ProductName, c.CategoryId, c.CategoryName
+    from Product p
+    join Category c ON p.CategoryId=c.CategoryId
+    order by p.ProductId ASC
+    limit ? offset ?`;
   db.query(sql, [pageSize, offset], callback);
 };
 
 exports.getAllCategories = (callback) => {
-  db.query("SELECT * FROM Category", callback);
+  db.query("select * from Category", callback);
 }; 
 
 exports.getById = (id, callback) => {
-  db.query("SELECT * FROM Product WHERE ProductId=?", [id], callback);
+  db.query("select * from Product where ProductId=?", [id], callback);
 };
 
 exports.create = (name, categoryId, callback) => {
-  db.query("INSERT INTO Product(ProductName, CategoryId) VALUES (?, ?)", [name, categoryId], callback);
+  db.query("insert into Product(ProductName, CategoryId) VALUES (?, ?)", [name, categoryId], callback);
 };
 
 exports.update = (id, name, categoryId, callback) => {
-  db.query("UPDATE Product SET ProductName=?, CategoryId=? WHERE ProductId=?", [name, categoryId, id], callback);
+  db.query("update Product set ProductName=?, CategoryId=? WHERE ProductId=?", [name, categoryId, id], callback);
 };
 
 exports.remove = (id, callback) => {
-  db.query("DELETE FROM Product WHERE ProductId=?", [id], callback);
+  db.query("delete from Product where ProductId=?", [id], callback);
 };
